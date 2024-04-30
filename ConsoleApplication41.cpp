08ad927e0ad3186d0d4afac2835b2cb8e14c65cc
popa2283 <127163765+popa2283@users.noreply.github.com>
2024-04-30 23:36:44 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication41.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+int main() {
+    std::string folderPath = "C:/Program Files (x86)";
+
+    for (const auto& entry : fs::directory_iterator(folderPath)) {
+        if (entry.is_directory()) {
+            std::cout << entry.path().filename() << std::endl;
+        }
+    }
+
+    return 0;
+}
